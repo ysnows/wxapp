187617cf6a711ba8d4774c72ec1c598e7c542cf5
咸光金 <ysnows@163.com>
2018-08-01 15:50:11 +0800

    1. 添加wxapp.jar

--- src/com/ysnows/wxapp/CreateFuncAction.java
+++ src/com/ysnows/wxapp/CreateFuncAction.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RelatedAction extends AnAction {
+public class CreateFuncAction extends AnAction {
 
 
     @Override

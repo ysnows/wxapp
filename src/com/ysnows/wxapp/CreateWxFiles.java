35b36b1bbd2c05a7117f33b728745e386d702fd6
咸光金 <ysnows@163.com>
2018-08-04 18:09:25 +0800

    1. 正在做创建pages功能

--- src/com/ysnows/wxapp/CreateWxFiles.java
+++ src/com/ysnows/wxapp/CreateWxFiles.java
@@ -2,12 +2,44 @@ package com.ysnows.wxapp;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.UIBundle;
+
+import java.io.File;
 
 public class CreateWxFiles extends AnAction {
 
+
     @Override
     public void actionPerformed(AnActionEvent e) {
-        // TODO: insert action logic here
+        String newFileName;
 
+        newFileName = Messages.showInputDialog(UIBundle.message("create.new.file.enter.new.file.name.prompt.text"),
+                UIBundle.message("new.file.dialog.title"), Messages.getQuestionIcon());
+        if (newFileName == null) {
+            return;
+        }
+        if ("".equals(newFileName.trim())) {
+            Messages.showMessageDialog(UIBundle.message("create.new.file.file.name.cannot.be.empty.error.message"),
+                    UIBundle.message("error.dialog.title"), Messages.getErrorIcon());
+        }
+
+
+        VirtualFile selectedFile = DataKeys.VIRTUAL_FILE.getData(e.getDataContext());
+        String path = selectedFile.getPath();
+
+        File file = new File(path, newFileName);
+        if (file.exists()) {
+            Utils.showErrorNotification(e.getProject(), "文件已存在");
+            return;
+        }
+        if (file.mkdirs()) {
+
+
+        }
     }
+
+
 }
